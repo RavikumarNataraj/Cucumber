6ea2e8db73fac1ceb69ffc90d022a2309becb3ba
Venkat59330 <venkatesan000008@gmail.com>
2022-03-26 20:58:22 +0530

    Testing

--- src/test/java/com/adact/stepdef/AdactLogin.java
+++ src/test/java/com/adact/stepdef/AdactLogin.java
@@ -22,6 +22,8 @@ public class AdactLogin {
 	@Given("open the adactin page")
 	public void open_the_adactin_page() {
 		driver.get("https://adactinhotelapp.com/");
+		
+		System.out.println("Venkat R");
 	}
 	
 	@When("Enter the {string} and {string}")
